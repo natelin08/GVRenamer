f3ef952b9ffe7d9e05717c8849e170507fc202e3
Nate <wwmonth@gmail.com>
2021-10-11 20:27:54 +0800

    v0.1版

---
+++ GVRenamer/Model/CseModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GVRenamer.Model
+{
+
+    public class CseModel
+    {
+        public string kind { get; set; }
+        public Url url { get; set; }
+        public Queries queries { get; set; }
+        public Context context { get; set; }
+        public Searchinformation searchInformation { get; set; }
+        public Item[] items { get; set; }
+    }
+
+    public class Url
+    {
+        public string type { get; set; }
+        public string template { get; set; }
+    }
+
+    public class Queries
+    {
+        public Request[] request { get; set; }
+    }
+
+    public class Request
+    {
+        public string title { get; set; }
+        public string totalResults { get; set; }
+        public string searchTerms { get; set; }
+        public int count { get; set; }
+        public int startIndex { get; set; }
+        public string inputEncoding { get; set; }
+        public string outputEncoding { get; set; }
+        public string safe { get; set; }
+        public string cx { get; set; }
+    }
+
+    public class Context
+    {
+        public string title { get; set; }
+    }
+
+    public class Searchinformation
+    {
+        public float searchTime { get; set; }
+        public string formattedSearchTime { get; set; }
+        public string totalResults { get; set; }
+        public string formattedTotalResults { get; set; }
+    }
+
+    public class Item
+    {
+        public string kind { get; set; }
+        public string title { get; set; }
+        public string htmlTitle { get; set; }
+        public string link { get; set; }
+        public string displayLink { get; set; }
+        public string snippet { get; set; }
+        public string htmlSnippet { get; set; }
+        public string formattedUrl { get; set; }
+        public string htmlFormattedUrl { get; set; }
+        public Pagemap pagemap { get; set; }
+    }
+
+    public class Pagemap
+    {
+        public Metatag[] metatags { get; set; }
+        public Article[] article { get; set; }
+    }
+
+    public class Metatag
+    {
+        public string referrer { get; set; }
+        public string themecolor { get; set; }
+        public string thumbnail { get; set; }
+        public string viewport { get; set; }
+        public string rating { get; set; }
+        public string google { get; set; }
+        public string category { get; set; }
+    }
+
+    public class Article
+    {
+        public string datemodified { get; set; }
+        public string text { get; set; }
+        public string headline { get; set; }
+    }
+
+}
---
+++ GVRenamer/Program.cs
@@ -0,0 +1,173 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using GVRenamer.Model;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text.Json;
+
+namespace GVRenamer
+{
+    internal class Program
+    {
+        static string FileExtensions = ".mp4,.avi,.rmvb,.wmv,.mov,.mkv,.flv,.ts,.webm,.iso,.mpg,.m4v";
+        static string WebUrl = "https://md.gvdb.org/search-for/#gsc.tab=0&gsc.q={0}%20&gsc.sort=";
+        static string OutputDir = "output";
+        static string FailDir = "fail";
+        static string DirectoryPath = Directory.GetCurrentDirectory();
+
+        static void Main(string[] args)
+        {
+            string[] allfiles = Directory.GetFiles(DirectoryPath, "*.*", SearchOption.TopDirectoryOnly);
+            allfiles = allfiles.Where(x => FileExtensions.Split(",").Contains(Path.GetExtension(x).ToLower())).ToArray();
+            foreach (var item in allfiles)
+            {
+                string fileName = Path.GetFileName(item);
+                string videoNumber = fileName.Substring(0, fileName.IndexOf("."));
+
+                string link = GetSearchLink(videoNumber);
+                string videoTitle = "";
+                if (!string.IsNullOrWhiteSpace(link))
+                {
+                    videoTitle = GetVideoTitle(link, videoNumber);
+                }
+                MoveFile(item, videoTitle);
+                if (!string.IsNullOrWhiteSpace(videoTitle))
+                {
+                    Console.WriteLine(videoNumber + " OK");
+                }
+            }
+            Console.WriteLine("處理檔案: " + allfiles.Length);
+            Console.WriteLine("請按任意鍵...");
+            Console.ReadLine();
+        }
+
+        public static void MoveFile(string item, string videoTitle)
+        {
+
+            if (!string.IsNullOrWhiteSpace(videoTitle))
+            {
+                string outputFileDir = Path.Combine(DirectoryPath, OutputDir + "/" + videoTitle);
+                Directory.CreateDirectory(outputFileDir);
+                File.Move(item, outputFileDir + "/" + videoTitle + Path.GetExtension(item));
+            }
+            else
+            {
+                string outputFileDir = Path.Combine(DirectoryPath, FailDir);
+                Directory.CreateDirectory(outputFileDir);
+                File.Move(item, outputFileDir + "/" + Path.GetFileName(item));
+            }
+        }
+
+        public static string GetSearchLink(string keyword)
+        {
+            string insideLink = "";
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--headless");
+
+            using (var driver = new ChromeDriver(".", options))
+            {
+                driver.Navigate().GoToUrl(String.Format(WebUrl, keyword));
+                try
+                {
+                    insideLink = driver.FindElementByXPath("//*[@id='___gcse_0']/div/div/div/div[5]/div[2]/div/div/div[1]/div/div[1]/div[1]/div/a").GetAttribute("href");
+                }
+                catch
+                {
+                    try
+                    {
+                        insideLink = driver.FindElementByXPath("//*[@id='___gcse_0']/div/div/div/div[5]/div[2]/div/div/div[2]/div[1]/div[1]/div[1]/div/a").GetAttribute("href");
+                    }
+                    catch {
+                        System.Console.WriteLine("查無資料: " + keyword);
+                    }
+                }
+            }
+
+            return insideLink;
+        }
+
+
+        public static string GetVideoTitle(string url, string videoNumber)
+        {
+            string fileName = "";
+            var config = Configuration.Default.WithDefaultLoader();
+            var dom = BrowsingContext.New(config).OpenAsync(url).Result;
+
+            var brandTitle = dom.QuerySelectorAll("div.entry-content > p");
+            var listCotentUl = dom.QuerySelectorAll("div.entry-content > ul");
+
+            int index = -1;
+            IElement queryItem = null;
+            foreach (var ul in listCotentUl)
+            {
+                index++;
+                var listCotentLi = ul.QuerySelectorAll("li");
+                queryItem = listCotentLi.FirstOrDefault(x => x.TextContent.Contains(videoNumber));
+                if (queryItem != null)
+                {
+                    break;
+                }
+            }
+
+            if (queryItem != null)
+            {
+                string brand = "";
+
+                int startIndex = brandTitle[0].TextContent.IndexOf("[ ") + 2;
+                int endIndex = brandTitle[0].TextContent.IndexOf(" ]");
+
+                if (endIndex < 0)
+                {
+                    endIndex = brandTitle[0].TextContent.IndexOf("]");
+                }
+
+                int length = endIndex - startIndex;
+                if (startIndex < 0 || endIndex < 0 || length < 0)
+                {
+                    index++;
+                }
+
+                startIndex = brandTitle[index].TextContent.IndexOf("[") + 2;
+                endIndex = brandTitle[index].TextContent.IndexOf(" ]");
+
+                if (endIndex < 0)
+                {
+                    endIndex = brandTitle[index].TextContent.IndexOf("]");
+                }
+
+                length = endIndex - startIndex;
+
+                brand = brandTitle[index].TextContent.Substring(startIndex, length);
+                brand = brand.Replace("[email protected]", "G@MES");
+
+                startIndex = queryItem.TextContent.IndexOf("] ") + 1;
+                endIndex = queryItem.TextContent.IndexOf(" (");
+                length = endIndex - startIndex;
+                string videoTitle = "";
+
+                if (length > 0)
+                {
+                    videoTitle = queryItem.TextContent.Substring(startIndex, length);
+                }
+                else
+                {
+                    videoTitle = queryItem.TextContent.Substring(startIndex);
+                }
+
+                if (videoTitle.Length > 40)
+                {
+                    videoTitle = videoTitle.Substring(0, 40) + "(...)";
+                }
+
+                fileName = string.Format("{0} [{1}] {2}", videoNumber, brand, videoTitle);
+            }
+
+            return fileName.Replace("  ", " ");
+        }
+    }
+}
